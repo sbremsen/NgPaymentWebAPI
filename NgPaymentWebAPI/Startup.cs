7189b44443b00eaf260317db12358d921e32a3cf
Scott Remsen <sbremsen@gmail.com>
2020-04-20 12:55:03 -0700

    Add project files.

---
+++ NgPaymentWebAPI/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NgPaymentWebAPI.Models;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NgPaymentWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+
+        {
+            services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                        .AddJsonOptions(options =>
+                            {
+                                // ignore default camel-case formatting applied to JSON response
+                                System.Text.Json.JsonNamingPolicy resolver = options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                            }
+                        );
+
+            services.AddDbContext<PaymentDetailContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            
+        }
+    }
+}
